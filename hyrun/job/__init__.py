c611a6b83ea108c108092e69f098a0efdc41cf2a
gdhjktru <gdhjktru@protonmail.com>
2025-05-26 01:32:33 +0200

    jo status

--- hyrun/job/__init__.py
+++ hyrun/job/__init__.py
@@ -2,6 +2,8 @@ from .get_workflow import get_workflow
 from .graph import JobGraph
 from .job import Job, get_job
 from .output import Output
+from .status import JobStatus, job_status_map
 
 __all__ = ['Job', 'get_job', 'Output',
-           'JobGraph', 'get_workflow']
+           'JobGraph', 'get_workflow',
+           'JobStatus', 'job_status_map']
--- hyrun/job/status.py
+++ hyrun/job/status.py
@@ -1,16 +1,20 @@
 from dataclasses import dataclass
 
 
+job_status_map = {'UNKNOWN': 0, 'PENDING': 10, 'RUNNING': 20, 'COMPLETED' : 30,
+                'BOOT_FAIL': 40, 'CANCELLED': 40, 'DEADLINE': 40, 'FAILED': 40,
+                'NODE_FAIL': 40, 'OUT_OF_MEMORY': 40, 'PREEMPTED': 40,
+                'TIMEOUT': 40}
+    
 @dataclass
 class JobStatus:
-
     """Dataclass containing information about the job status."""
-
 
     job_id: str
     job_name: str
     submission_time: str
-    state: str
+    status: str
     quality_of_service: str
     elapsed_time: str
     time_left: str
+
